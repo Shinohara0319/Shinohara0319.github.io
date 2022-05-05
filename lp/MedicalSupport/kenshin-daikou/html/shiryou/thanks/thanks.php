3581f1a87c6589f98a2eaa230323be0463f47d78
yoshinori.shinohara <yoshinori.shinohara@MV-LT-042noMacBook-puro.local>
2022-05-05 16:40:30 +0900

    自動ダウンロード開始するmetaタグとファイル設置

--- lp/MedicalSupport/kenshin-daikou/html/shiryou/thanks/thanks.php
+++ lp/MedicalSupport/kenshin-daikou/html/shiryou/thanks/thanks.php
@@ -24,6 +24,7 @@ if (
   <meta charset="UTF-8" />
   <meta name="viewport" content="width-device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <meta http-equiv="Refresh" content="1;URL=../../../download-document/text.txt.zip">
   <title>ダウンロードありがとうございました。 Medical Support</title>
   <meta name="description" content="ダウンロードありがとうございました。 Medical Support" />
   <meta name="keywords" content="健診代行ならメイサービス Medical Support" />
